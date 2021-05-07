c26c608d42997b0f7ab82398262881e25e6b3bd0
aylamar <aylamar@users.noreply.github.com>
2021-05-07 12:39:42 -0500

    Filled out cookPriceDB with all IDs & names

---
+++ modules/cookPriceDB.js
@@ -0,0 +1,628 @@
+priceDB = {
+    'Aloe': {
+        id: 7347,
+        value: 930
+    },
+    'Beer': {
+        id: 9213,
+        value: 2530
+    },
+    'Kuku Bird Meat': {
+        id: 7916,
+        value: 4780
+    },
+    'Flamingo Meat': {
+        id: 7917,
+        value: 5250
+    },
+    'Chicken Meat': {
+        id: 7921,
+        value: 1350
+    },
+    'Blue Whale Meat': {
+        id: 9726,
+        value: 555000
+    },
+    'Boiled Bird Eggs': {
+        id: 9402,
+        value: 3050
+    },
+    'Bracken': {
+        id: 5532,
+        value: 6150
+    },
+    'Butter': {
+        id: 9063,
+        value: 3250
+    },
+    'Butter-Roasted Lobster': {
+        id: 9477,
+        value: 52500
+    },
+    'Cheese': {
+        id: 9062,
+        value: 6100
+    },
+    'Cheese Gratin': {
+        id: 9203,
+        value: 26400
+    },
+    'Cheese Pie': {
+        id: 9268,
+        value: 14000
+    },
+    'Cinnamon': {
+        id: 7348,
+        value: 408
+    },
+    'Coconut': {
+        id: 7026,
+        value: 3510
+    },
+    'Coconut Cocktail': {
+        id: 9307,
+        value: 5650
+    },
+    'Coconut Fried Fish': {
+        id: 9309,
+        value: 9500
+    },
+    'Coconut Pasta': {
+        id: 9308,
+        value: 13400
+    },
+    'Cooking Honey': {
+        id: 7702,
+        value: 1900
+    },
+    'Wild Beehive': {
+        id: 7701,
+        value: 23300
+    },
+    'Wheat': {
+        id: 7001,
+        value: 1590
+    },
+    'Barley': {
+        id: 7002,
+        value: 1590
+    },
+    'Potato': {
+        id: 7003,
+        value: 1410
+    },
+    'Sweet Potato': {
+        id: 7004,
+        value: 1410
+    },
+    'Corn': {
+        id: 7005,
+        value: 1670
+    },
+    'Wheat Dough': {
+        id: 7201,
+        value: 1340
+    },
+    'Barley Dough': {
+        id: 7202,
+        value: 1430
+    },
+    'Potato Dough': {
+        id: 7203,
+        value: 1350
+    },
+    'Sweet Potato Dough': {
+        id: 7204,
+        value: 1400
+    },
+    'Corn Dough': {
+        id: 7205,
+        value: 1460
+    },
+    'Wheat Flour': {
+        id: 7101,
+        value: 1770
+    },
+    'Barley Flour': {
+        id: 7102,
+        value: 1670
+    },
+    'Potato Flour': {
+        id: 7103,
+        value: 1820
+    },
+    'Sweet Potato Flour': {
+        id: 7104,
+        value: 2120
+    },
+    'Corn Flour': {
+        id: 7105,
+        value: 1650
+    },
+    'Couscous': {
+        id: 9220,
+        value: 18400
+    },
+    'Cream': {
+        id: 9061,
+        value: 5850
+    },
+    'Dark Pudding': {
+        id: 9422,
+        value: 43700
+    },
+    'Date Palm': {
+        id: 7016,
+        value: 2980
+    },
+    'Date Palm Wine': {
+        id: 9219,
+        value: 6100
+    },
+    'Deer Blood': {
+        id: 6201,
+        value: 10100
+    },
+    'Sheep Blood': {
+        id: 6202,
+        value: 9250
+    },
+    'Pig Blood': {
+        id: 6205,
+        value: 8350
+    },
+    'Waragon Blood': {
+        id: 6215,
+        value: 12300
+    },
+    'Ox Blood': {
+        id: 6206,
+        value: 7950
+    },
+    'Dressing': {
+        id: 9006,
+        value: 705
+    },
+    'Dried Pearl Oyster Flesh': {
+        id: 6520,
+        value: 60500
+    },
+    'Tridana Giant Clam Shellfish': {
+        id: 6535,
+        value: 127000
+    },
+    'Egg': {
+        id: 9064,
+        value: 4750
+    },
+    'Essence of Liquor': {
+        id: 9057,
+        value: 615
+    },
+    'Exotic Herbal Wine': {
+        id: 9262,
+        value: 1290
+    },
+    'Fig': {
+        id: 7018,
+        value: 2950
+    },
+    'Fig Pie': {
+        id: 9216,
+        value: 7850
+    },
+    'Fish Fillet Salad': {
+        id: 9423,
+        value: 11700
+    },
+    'Fortune Teller Mushroom': {
+        id: 5407,
+        value: 700
+    },
+    'Fragrant Jerky': {
+        id: 7918,
+        value: 4400
+    },
+    'Freekeh': {
+        id: 7021,
+        value: 1350
+    },
+    'Freekeh Snake Stew': {
+        id: 9215,
+        value: 22500
+    },
+    'Fried Bird': {
+        id: 9403,
+        value: 6900
+    },
+    'Strawberry': {
+        id: 7304,
+        value: 545
+    },
+    'Grape': {
+        id: 7307,
+        value: 1840
+    },
+    'Apple': {
+        id: 7313,
+        value: 775
+    },
+    'Cherry': {
+        id: 7314,
+        value: 775
+    },
+    'Pear': {
+        id: 7315,
+        value: 775
+    },
+    'Banana': {
+        id: 7316,
+        value: 775
+    },
+    'Pineapple': {
+        id: 7317,
+        value: 775
+    },
+    'Fruit and Vegetable Salad': {
+        id: 9258,
+        value: 1390
+    },
+    'Fruit Wine': {
+        id: 9201,
+        value: 1740
+    },
+    "Fugitive Khalk's Horn": {
+        id: 9735,
+        value: 277000
+    },
+    'Full-bodied Exotic Herbal Wine': {
+        id: 9279,
+        value: 2720
+    },
+    'Garlic': {
+        id: 7302,
+        value: 4190
+    },
+    'Ghormeh Sabzi': {
+        id: 9486,
+        value: 25400
+    },
+    'Grilled Sausage': {
+        id: 9427,
+        value: 17100
+    },
+    'Grilled Scorpion': {
+        id: 9461,
+        value: 25200
+    },
+    'Ground Bird Meat': {
+        id: 7958,
+        value: 2570
+    },
+    'Ham Sandwich': {
+        id: 9267,
+        value: 37300
+    },
+    'High-Quality Carrot': {
+        id: 54004,
+        value: 1160
+    },
+    'Honey Wine': {
+        id: 9206,
+        value: 1490
+    },
+    'Honeycomb Cookie': {
+        id: 9266,
+        value: 18700
+    },
+    'Hot Pepper': {
+        id: 7305,
+        value: 2560
+    },
+    'King of Jungle Hamburg': {
+        id: 9463,
+        value: 42100
+    },
+    'Lean Meat Salad': {
+        id: 9425,
+        value: 17500
+    },
+    'Lion Meat': {
+        id: 7923,
+        value: 13600
+    },
+    'Lizard Meat': {
+        id: 7908,
+        value: 8500
+    },
+    'Worm Meat': {
+        id: 7909,
+        value: 6150
+    },
+    'Waragon Meat': {
+        id: 7914,
+        value: 5800
+    },
+    'Llama Meat': {
+        id: 7956,
+        value: 9700
+    },
+    'Lump of Raw Sugar': {
+        id: 54003,
+        value: 2700
+    },
+    'Marmot Meat': {
+        id: 7955,
+        value: 9350
+    },
+    'Deer Meat': {
+        id: 7901,
+        value: 9250
+    },
+    'Fox Meat': {
+        id: 7903,
+        value: 6150
+    },
+    'Rhino Meat': {
+        id: 7904,
+        value: 11900
+    },
+    'Pork': {
+        id: 7905,
+        value: 7500
+    },
+    'Beef': {
+        id: 7906,
+        value: 7050
+    },
+    'Raccoon Meat': {
+        id: 7910,
+        value: 7950
+    },
+    'Weasel Meat': {
+        id: 7911,
+        value: 5300
+    },
+    'Bear Meat': {
+        id: 7912,
+        value: 10100
+    },
+    'Wolf Meat': {
+        id: 7913,
+        value: 8800
+    },
+    'Meat Croquette': {
+        id: 9404,
+        value: 28600
+    },
+    'Meat Pasta': {
+        id: 9416,
+        value: 19900
+    },
+    'Meat Pie': {
+        id: 9265,
+        value: 13600
+    },
+    'Meat Stew': {
+        id: 9414,
+        value: 11900
+    },
+    'Mediah Meal': {
+        id: 9607,
+        value: 41300
+    },
+    'Milk': {
+        id: 9065,
+        value: 10100
+    },
+    'Milk Tea': {
+        id: 9263,
+        value: 23900
+    },
+    'Nutmeg': {
+        id: 7020,
+        value: 3700
+    },
+    'Oatmeal': {
+        id: 9261,
+        value: 16300
+    },
+    'Onion': {
+        id: 7303,
+        value: 4190
+    },
+    'Oyster': {
+        id: 6515,
+        value: 19600
+    },
+    'Pan-Fried Oyster': {
+        id: 9475,
+        value: 33300
+    },
+    'Pepper': {
+        id: 7301,
+        value: 2380
+    },
+    'Pickled Vegetables': {
+        id: 9202,
+        value: 1970
+    },
+    'Pistachio': {
+        id: 7017,
+        value: 670
+    },
+    'Prawn Salad': {
+        id: 9471,
+        value: 6200
+    },
+    'Purified Water': {
+        id: 6656,
+        value: 4960
+    },
+    'Rainbow Button Mushroom': {
+        id: 5515,
+        value: 7700
+    },
+    'Rainbow Button Mushroom Cheese Melt': {
+        id: 9483,
+        value: 25200
+    },
+    'Rainbow Button Mushroom Sandwich': {
+        id: 9316,
+        value: 33500
+    },
+    'Red Sauce': {
+        id: 9004,
+        value: 4980
+    },
+    'Roast Marmot': {
+        id: 9488,
+        value: 6450
+    },
+    'Sunflower': {
+        id: 7308,
+        value: 3350
+    },
+    'Rose': {
+        id: 7319,
+        value: 3510
+    },
+    'Tulip': {
+        id: 7320,
+        value: 3510
+    },
+    'Scorpion Meat': {
+        id: 7924,
+        value: 9250
+    },
+    'Serendia Meal': {
+        id: 9603,
+        value: 27000
+    },
+    'Shrimp': {
+        id: 6516,
+        value: 12700
+    },
+    'Skewered Llama Cheese Melt': {
+        id: 9490,
+        value: 19300
+    },
+    'Smoked Fish Steak': {
+        id: 9417,
+        value: 2850
+    },
+    'Snake Meat': {
+        id: 7922,
+        value: 11500
+    },
+    'Soft Bread': {
+        id: 9260,
+        value: 18700
+    },
+    'Soft Whale Meat': {
+        id: 9731,
+        value: 20200
+    },
+    'Crocodile Meat': {
+        id: 9732,
+        value: 20200
+    },
+    'Special Carrot': {
+        id: 54005,
+        value: 9500
+    },
+    'Special Drieghanese Meal': {
+        id: 9637,
+        value: 41800
+    },
+    'Star Anise': {
+        id: 7019,
+        value: 670
+    },
+    'Steak': {
+        id: 9401,
+        value: 16100
+    },
+    'Steamed Prawn': {
+        id: 9473,
+        value: 28700
+    },
+    'Stir-Fried Bracken': {
+        id: 9318,
+        value: 19000
+    },
+    'Stir-Fried Vegetables': {
+        id: 9241,
+        value: 6700
+    },
+    'Tea With Fine Scent': {
+        id: 9270,
+        value: 6650
+    },
+    'Teff': {
+        id: 7022,
+        value: 795
+    },
+    'Teff Bread': {
+        id: 9214,
+        value: 2920
+    },
+    'Teff Flour': {
+        id: 7106,
+        value: 1350
+    },
+    'Teff Flour Dough': {
+        id: 7206,
+        value: 1790
+    },
+    'Teff Sandwich': {
+        id: 9218,
+        value: 31200
+    },
+    'Top-Quality Cooking Honey': {
+        id: 7704,
+        value: 238000
+    },
+    'Vinegar': {
+        id: 9066,
+        value: 785
+    },
+    'Wheat': {
+        id: 7001,
+        value: 1590
+    },
+    'White Sauce': {
+        id: 9003,
+        value: 2310
+    },
+    'Borscht': {
+        id: 9408,
+        value: 15300
+    },
+    'Fragrant Borscht': {
+        id: 9452,
+        value: 27700
+    }
+};
+
+module.exports = priceDB;
+
+/* Used to fetch all cooking IDs and print all data to the console 
+const fetch = require('node-fetch');
+
+var jsonArray = [];
+
+async function getPrice() {
+    var cookIngredients = await fetch(`https://bdo-api-helper.herokuapp.com/api/prices/cooking?region=na`);
+    var cookIngredParsed = await cookIngredients.json();
+
+    cookIngredParsed.forEach(element => {
+
+        jsonArray.push(element.name, {id: element.id, value: element.price})
+
+    });
+    console.dir(jsonArray, {'maxArrayLength': null})
+}
+
+getPrice();
+*/
--- modules/priceUpdater.js
+++ modules/priceUpdater.js
@@ -5,8 +5,6 @@ var prodPriceDB = require('./prodPriceDB');
 
 async function priceUpdater() {
     console.log('Gathering Prices...');
-    //console.log(await getPrice(priceDB['Maple Timber'].id));
-
     for (var key in prodPriceDB) {
 
         //Function information:
@@ -21,6 +19,20 @@ async function priceUpdater() {
 
     }
     console.log('Done gathering production prices!')
+
+    // Gather
+    /*var cookIngredients = await fetch(`https://bdo-api-helper.herokuapp.com/api/prices/cooking?region=na`);
+    var cookIngredParsed = await cookIngredients.json();
+
+    cookIngredParsed.forEach(element => {
+        console.log(element.name, element.id, element.price);
+
+        cookPriceDB[element.name].price = element.price;
+
+    });
+    console.log('Done gathering cooking ingredient prices!')
+    */
+    
 }
 
 
