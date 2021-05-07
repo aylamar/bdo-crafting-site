726c6c0151c7acc98f1cd250aebe59423c8c0637
aylamar <aylamar@users.noreply.github.com>
2021-05-07 13:52:46 -0500

    Added dotenv for easier testing during development

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const priceUpdater = require('./modules/priceUpdater')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 // Import Routers
 const indexRouter = require('./routes/index');
@@ -30,5 +33,5 @@ app.use('/updates', updatesRouter)
 app.listen(process.env.PORT || 80);
 
 // Update prices, then update prices every 15 minutes.
-setTimeout(priceUpdater, 5000);
+setTimeout(priceUpdater, process.env.DELAY || 5000);
 setInterval(priceUpdater, 900000);
