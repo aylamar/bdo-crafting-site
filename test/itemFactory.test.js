7a17653d8938cf00947ae4e72166e7efb889d8d5
aylamar <aylamar@users.noreply.github.com>
2021-05-12 00:40:31 -0500

    Improved testing

--- test/itemFactory.test.js
+++ test/itemFactory.test.js
@@ -2,7 +2,7 @@
 const assert = require('assert');
 const { expect } = require('chai');
 const { itemFactory } = require('../modules/itemFactory/itemFactory');
-const { balenosDefault, steelDefault, balenosMealBody, balenosMealOutput, advCookUtensilBody, advCookUtensilOutput, serendiaTimberBody, serendiaTimberOutput, pickedVegetablesDefault } = require('./testData');
+const { balenosDefault, steelDefault, balenosMealBody, balenosMealOutput, advCookUtensilBody, advCookUtensilOutput, serendiaTimberBody, serendiaTimberOutput } = require('./testData');
 
 // Guide: https://www.youtube.com/watch?v=k4GFqgBR2qc
 
@@ -34,6 +34,27 @@ describe('the itemFactory', function() {
 
     it('should generate valid pickled vegetables data', function() {
         var data = itemFactory("Pickled_Vegetables", 'cooking', null);
-        expect(data).to.deep.equal(data, pickedVegetablesDefault);
+        
+        // Spot check tree
+        expect(data.materialTree[4]).to.have.all.keys('name', 'imageName', 'column', 'count', 'totalCount', 'multiPart');
+        expect(data.materialTree[0].name).to.equal('Leavening Agent');
+        expect(data.materialTree[2].name).to.equal('Paprika');
+        expect(data.materialTree[0].totalCount).to.equal('2');
+        expect(data.materialTree[2].totalCount).to.equal('8');
+
+        // Spot check material list
+        expect(data.materialList[3]).to.have.all.keys('batchCost', 'count', 'name', 'cost');
+        expect(data.materialList[2].name).to.equal('Paprika');
+        expect(data.materialList[4].name).to.equal('Grain');
+        expect(data.materialList[2].count).to.equal(8);
+        expect(data.materialList[4].count).to.equal(2);
+
+        // Spot check proc list
+        expect(data.procList[0]).to.have.all.keys('name', 'count', 'cost', 'batchCost');
+
+        // Spot check profit
+        expect(data.profit.taxableProcBatch).to.equal('8,360');
+        expect(data.profit.profitBatch).to.equal('7,064');
+
     })
 });
