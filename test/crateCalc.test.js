64b84dfd38ef83199249b6af60010889b57265f1
aylamar <aylamar@users.noreply.github.com>
2021-05-11 22:39:45 -0500

    Added Chai for testing

--- test/crateCalc.test.js
+++ test/crateCalc.test.js
@@ -9,21 +9,21 @@ const { balenosDefault, steelDefault, redSauceDefault, odyDefault, advCookUtensi
 describe('the itemFactory', function() {
     it('should have valid default balenos timber crate data', function() {
         var data = itemFactory('Balenos_Timber_Crate', 'production', null);
-        assert.notStrictEqual(data, balenosDefault);
+        expect(data).to.deep.equal(data, balenosDefault);
     })
 
     it('should have valid default steel ingot crate data', function() {
         var data = itemFactory('Steel_Ingot_Crate', 'production', null);
-        assert.notStrictEqual(data, steelDefault);
+        expect(data).to.deep.equal(steelDefault);
     })
 
     it('should have valid advanced cooking utensil data', function() {
         var data = itemFactory("Advanced_Cooking_Utensil", 'production', advCookUtensilBody);
-        assert.notStrictEqual(data, advCookUtensilOutput);
+        expect(data).to.deep.equal(data, advCookUtensilOutput);
     })
 
     it('should have valid serendia timber crate data', function() {
         var data = itemFactory("Advanced_Cooking_Utensil", 'production', serendiaTimberBody);
-        assert.notStrictEqual(data, serendiaTimberOutput);
+        expect(data).to.deep.equal(data, serendiaTimberOutput);
     })
 });
