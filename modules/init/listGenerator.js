a2113b9b88a7290167f1b8c63a47a8e4f6f1c577
aylamar <aylamar@users.noreply.github.com>
2021-05-17 13:27:37 -0500

    Updated tests for new icon location

--- modules/init/listGenerator.js
+++ modules/init/listGenerator.js
@@ -41,7 +41,7 @@ var linkGenerator = function linkGenerator (val, type) {
 };
 
 var imgGenerator = function imgGenerator (val) {
-    return 'assets/icon/' + val.toLowerCase().replace(/ /g,"-") + ".png";
+    return '/assets/icon/' + val.toLowerCase().replace(/ /g,"-") + ".png";
 };
 
 module.exports = {listGenerator, cookListGenerator, prodListGenerator, linkGenerator, imgGenerator};
